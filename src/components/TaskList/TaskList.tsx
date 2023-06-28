62706f0cc91f90cab1cebfa68ff3f783636bee53
Wilson Santos <contato@wilsonsantos.net>
2023-06-27 21:17:52 -0300

    atualiza a lista hardcoded

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,42 +1,38 @@
 import { List } from "@mui/material";
 import ItemList from "./ItemList";
+import { useState } from "react";
 
-interface IProps{
-  tasks : string[],
+interface IProps {
+  tasks: string[];
 }
 
-
-function TaskList({tasks}: IProps){
-  const tarefas = [
+function TaskList({ tasks }: IProps) {
+  const [tarefas, setTarefas] = useState([
     {
-    task: "teste",
-    time: "00:00:00"
+      task: "teste",
+      time: "00:00:00",
     },
     {
-    task: "teste2",
-    time: "00:10:00"
+      task: "teste2",
+      time: "00:10:00",
     },
     {
-    task: "teste3",
-    time: "00:20:00"
+      task: "teste3",
+      time: "00:20:00",
     },
     {
-    task: "teste4",
-    time: "00:30:00"
+      task: "teste4",
+      time: "00:30:00",
     },
-]
-  return(
+  ]);
+  return (
     <>
-      <h2>Tarefas</h2>
-      <List
-      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-      >
-      {tarefas.map((item, index) => (
-                <ItemList key={index} task={item.task} time={item.time}/>
-              ))}
+      <h2 onClick={()=>{ setTarefas([...tarefas,{task:"nova tarefa",time:"04:03:01"}]) }}>Tarefas</h2>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        {tarefas.map((item, index) => (
+          <ItemList key={index} task={item.task} time={item.time} />
+        ))}
       </List>
-        
-      
     </>
   );
 }
